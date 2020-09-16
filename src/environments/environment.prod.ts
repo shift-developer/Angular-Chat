4d00f8a66570bdbaf865cd5d8de22f9974db72be
Juan Shift <61192769+shift-developer@users.noreply.github.com>
2020-09-15 21:48:02 -0300

    i

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB-Ez8P5dMYnAqpPO6L-mVBYVyJXfrV1MQ',
+    authDomain: 'firechat-angular-7ec7f.firebaseapp.com',
+    databaseURL: 'https://firechat-angular-7ec7f.firebaseio.com',
+    projectId: 'firechat-angular-7ec7f',
+    storageBucket: 'firechat-angular-7ec7f.appspot.com',
+    messagingSenderId: '71000600023',
+    appId: "1:71000600023:web:80ecc239e6cdc15a606f92",
+    measurementId: "G-84GTTBQZH5"
+  }
 };
